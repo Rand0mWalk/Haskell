efa6f4a2487707ab1eb9bf8485c6f47b84a595fc
Rand0mW4lk <nuh4u87+randomwalk@gmail.com>
2022-02-27 15:24:20 +0400

    Added idiomatic pattern matching

--- ch6/9-funcs.hs
+++ ch6/9-funcs.hs
@@ -1,4 +1,4 @@
-sum' :: [Int] -> Int
+sum' :: Num p => [p] -> p
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
 
@@ -10,3 +10,8 @@ take' n (x:xs) = x : take' (n-1) xs
 lst :: [a] -> a
 lst (x:xs) | null xs = x
            | otherwise = lst xs
+
+--Using idiomatic pattern matching
+lsta :: [a] -> a
+lsta [x] = x
+lsta (x:xs) = lsta xs
