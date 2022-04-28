3efb1bcc0d92b0f8661e452d98bd8270657e77a1
Rand0mW4lk <nuh4u87+randomwalk@gmail.com>
2022-04-28 13:53:23 +0400

    Binary Transmission with Parity check

---
+++ ch7/7-binStringTransmitter.hs
@@ -0,0 +1,31 @@
+import Data.Char --to use ord
+type Bit = Int
+
+bin2int :: [Bit] -> Int
+bin2int = foldr (\acc x -> acc + 2*x) 0
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+addparity :: [Bit] -> [Bit]
+addparity bits | (odd . length . filter (==1)) bits = bits ++ [1]
+               | otherwise  = bits ++ [0]
+
+encode :: String -> [Bit]
+encode = concat . map (addparity . make8 . int2bin . ord)
+
+chop9 :: [Bit] -> [[Bit]]
+chop9 [] = []
+chop9 bits = take 9 bits : chop9 (drop 9 bits)
+
+checkparity :: [Bit] -> [Bit]
+checkparity bits | (odd . length . filter (==1) . take 8) bits && (bits !! 8 == 1)  = take 8 bits
+                 | (even . length . filter (==1) . take 8) bits && (bits !! 8 == 0)  = take 8 bits
+                 | otherwise = error "Error in parity bits, aborting transmission"
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int . checkparity) . chop9  
